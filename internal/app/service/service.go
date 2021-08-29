61ba95689f14dd7e60fca24bc50a150d223506b1
Md Riadul Islam <rdhawladar@gmail.com>
2021-08-29 11:07:20 +0600

    AP-14 | Generated mock file and removed single line comments

--- internal/app/service/service.go
+++ internal/app/service/service.go
@@ -1,3 +1,5 @@
+//go:generate mockgen -source=internal/app/service/service.go -destination=internal/app/service/mocks/mock_service.go
+
 package service
 
 import (
@@ -5,26 +7,22 @@ import (
 	"parcel-service/internal/app/model"
 )
 
-// ParcelRepository to Insert New Parcel
 type ParcelRepository interface {
 	InsertParcel(ctx context.Context, parcel model.Parcel) error
 	FetchParcelByID(ctx context.Context, parcelID int) (model.Parcel, error)
 	UpdateParcel(ctx context.Context, parcel model.Parcel) error
 }
 
-// ParcelService to Create new parcel
 type ParcelService interface {
 	CreateParcel(ctx context.Context, parcel model.Parcel) error
 	GetParcelByID(ctx context.Context, parcelID int) (model.Parcel, error)
 	EditParcel(ctx context.Context, parcel model.Parcel) error
 }
 
-// CarrierRepository to update the carrier request table
 type CarrierRepository interface {
 	InsertCarrierRequest(ctx context.Context, carrierReq model.CarrierRequest) error
 }
 
-// CarrierService to add new carrier request
 type CarrierService interface {
 	NewCarrierRequest(ctx context.Context, carrierReq model.CarrierRequest) error
 }
